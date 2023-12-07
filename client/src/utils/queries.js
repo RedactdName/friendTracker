05f86f14cf619b7925bd17a006d4c75910ff4722
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-12-07 13:52:55 -0700

    added to profileSchema in Profile.js, added mutations & queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,31 +1,55 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PROFILES = gql`
-  query allProfiles {
-    profiles {
-      _id
-      name
-      skills
-    }
-  }
-`;
+// export const QUERY_PROFILES = gql`
+//   query allProfiles {
+//     profiles {
+//       _id
+//       name
+//       skills
+//     }
+//   }
+// `;
 
-export const QUERY_SINGLE_PROFILE = gql`
-  query singleProfile($profileId: ID!) {
-    profile(profileId: $profileId) {
-      _id
-      name
-      skills
-    }
-  }
-`;
+// export const QUERY_SINGLE_PROFILE = gql`
+//   query singleProfile($profileId: ID!) {
+//     profile(profileId: $profileId) {
+//       _id
+//       name
+//       skills
+//     }
+//   }
+// `;
 
 export const QUERY_ME = gql`
   query me {
     me {
       _id
-      name
-      skills
+      username
+      location {
+        lat
+        lon
+      }
+    }
+  }
+`;
+// ________________________________________________
+export const QUERY_FRIENDS = gql`
+  query getFriends {
+    getFriends {
+      _id
+      username
+      location {
+        lat
+        lon
+      }
+    }
+  }
+`;
+export const QUERY_LOCATION = gql`
+  query getLocation {
+    getLocation {
+      lat
+      lon
     }
   }
 `;
