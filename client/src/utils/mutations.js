9d0cc6b9b17c10e2efa72e80ef7165faaa1826b6
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-12-11 19:20:17 -0700

    deleted skills, changed wori ng in Profilelist index to reflect skills change to friends, update mutati ons for friends

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,21 +27,10 @@ export const LOGIN_USER = gql`
 
 
 // ________________________________________________ added by LE
-// export const ADD_USER = gql`
-//   mutation addUser($name: String!, $email: String!, $password: String!, $username: String!) {
-//     addUser(name: $name, email: $email, password: $password, username: $username) {
-//       token
-//       profile {
-//         _id
-//         name
-//       }
-//     }
-//   }
-// `;
 
 export const ADD_FRIEND = gql`
-  mutation addFriend($friendId: ID!) {
-    addFriend(friendId: $friendId) {
+  mutation addFriend($friendId: ID!, profileId: ID!) {
+    addFriend(friendId: $friendId, profileId: $profileId) {
       _id
       name
       friends
@@ -49,54 +38,54 @@ export const ADD_FRIEND = gql`
   }
 `;
 
-// export const REMOVE_FRIEND = gql`
-//   mutation removeFriend($friendId: ID!) {
-//     removeFriend(friendId: $friendId) {
-//       _id
-//       name
-//       friends
-//     }
-//   }
-// `;
+export const REMOVE_FRIEND = gql`
+  mutation removeFriend($friendId: ID!, profileId: ID!) {
+    removeFriend(friendId: $friendId, profileId: $profileId) {
+      _id
+      name
+      friends
+    }
+  }
+`;
 
-// export const REMOVE_USER = gql`
-//   mutation removeUser($userId: ID!) {
-//     removeUser(userId: $userId) {
-//       _id
-//       name
-//     }
-//   }
-// `;
+export const REMOVE_USER = gql`
+  mutation removeUser($userId: ID!, profileId: ID!) {
+    removeUser(userId: $userId, profileId: $profileId) {
+      _id
+      name
+    }
+  }
+`;
 
 
-// export const ADD_GROUP = gql`
-//   mutation addGroup($groupId: ID!) {
-//     addGroup(groupId: $groupId) {
-//       _id
-//       name
-//       groups
-//     }
-//   }
-// `;
+export const ADD_GROUP = gql`
+  mutation addGroup($groupId: ID!) {
+    addGroup(groupId: $groupId) {
+      _id
+      name
+      groups
+    }
+  }
+`;
 
-// export const REMOVE_GROUP = gql`
-//   mutation removeGroup($groupId: ID!) {
-//     removeGroup(groupId: $groupId) {
-//       _id
-//       name
-//       groups
-//     }
-//   }
-// `;
+export const REMOVE_GROUP = gql`
+  mutation removeGroup($groupId: ID!) {
+    removeGroup(groupId: $groupId) {
+      _id
+      name
+      groups
+    }
+  }
+`;
 
-// export const UPDATE_GROUP = gql`
-//   mutation updateGroup($groupId: ID!, $name: String!) {
-//     updateGroup(groupId: $groupId, name: $name) {
-//       _id
-//       name
-//     }
-//   }
-// `;
+export const UPDATE_GROUP = gql`
+  mutation updateGroup($groupId: ID!, $name: String!) {
+    updateGroup(groupId: $groupId, name: $name) {
+      _id
+      name
+    }
+  }
+`;
 
 export const UPDATE_LOCATION = gql`
 mutation updateLocation($profileId: ID!, $lat: Float!, $lon: Float!) {
