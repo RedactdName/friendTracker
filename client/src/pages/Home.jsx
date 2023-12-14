5370b74a1b703335cf90917f1e7591a5f9cc5362
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-12-14 11:43:13 -0700

    changed homepage image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,11 +15,8 @@ const Home = () => {
           {loading ? (
             <div>Loading...</div>
           ) : (
-            < img src="https://images.unsplash.com/photo-1582298538104-fe2e74c27f59?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="logo" className='img-fluid mx-auto rounded h-25 d-inline-block'/>
-            // ProfileList
-            //   profiles={profiles}
-            //   title="Here's the current roster of friends..."
-            // />
+            < img src="../src/assets/DucksInAPark.png" alt="Rubber Ducks set in a serene park background" className='img-fluid mx-auto rounded h-25 d-inline-block'/>
+            
           )}
         </div>
       </div>
