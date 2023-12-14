537441dbb54825aaa6e0b2ec004ba897052b3231
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-12-14 09:51:52 -0700

    adding about.jsx

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,12 @@
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        This is a social media app that allows users to create an account, add friends to a list and track their movements while signed in The purpose was to make those rougher nights out a little easier to follow the breadcrumbs of that one friend who keeps getting lost.
+        </p>
+    </div>
+  );
+}
+export default About;
