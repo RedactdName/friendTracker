051ef02e604bd2c92ca5abff58dc649e15e64fc3
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-12-14 10:05:35 -0700

    add selves as contributors in About

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,6 +6,11 @@ function About() {
       <p>
         This is a social media app that allows users to create an account, add friends to a list and track their movements while signed in The purpose was to make those rougher nights out a little easier to follow the breadcrumbs of that one friend who keeps getting lost.
         </p>
+        <ul>Contributors
+        <li><a href="https://github.com/RedactdName">Lisa Eimicke</a></li>
+        <li><a href="https://github.com/Cehura-Code">Brittney Cehura</a></li>
+        <li><a href="https://github.com/ccoff1798">Codey Coffey</a></li>
+        </ul >
     </div>
   );
 }
